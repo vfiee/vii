04dea42f3dc39021ceb461e0c44c8212da5e4e02
vyron <fiee_vyron@163.com>
2025-03-07 23:54:06 +0800

    release: release-scripts@0.0.2

--- scripts/release.ts
+++ scripts/release.ts
@@ -3,7 +3,9 @@ import { logRecentCommits, run } from './releaseUtils'
 
 release({
   repo: 'vii',
-  packages: ['cli', 'shared'],
+  ignorePublint: true,
+  ignoreReleaseConfirm: true,
+  packages: ['cli', 'shared', 'release-scripts'],
   toTag: (pkg, version) => `${pkg}@${version}`,
   logChangelog: (pkg) => logRecentCommits(pkg),
   generateChangelog: async (pkgName) => {
