b7cca8eafabc82d4a2b7a9da5c9e7db8326069b1
vyron <fiee_vyron@163.com>
2025-04-03 17:20:07 +0800

    feat: change useAxios to use-axios

--- scripts/release.ts
+++ scripts/release.ts
@@ -5,7 +5,7 @@ release({
   repo: 'vii',
   ignorePublint: true,
   ignoreReleaseConfirm: true,
-  packages: ['cli', 'utils', 'release-scripts', 'useAxios'],
+  packages: ['cli', 'utils', 'release-scripts', 'use-axios'],
   toTag: (pkg, version) => `${pkg}@${version}`,
   logChangelog: (pkg) => logRecentCommits(pkg),
   generateChangelog: async (pkgName) => {
